c4927d7ef3a5c9b0555147194b05e2bdd801daa5
toom pornpavit <pornpavit@odds.team>
2019-05-08 16:05:18 +0700

    Add function calculate in server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const handlerApp = (req, res) => {
     console.log(req.params.name)
     res.send(`hello  ` + req.params.name)
 }
-
 app.get('/app/:name', handlerApp)
+
+const calculate = (req, res) => {
+    console.log(req.params);
+    const a = req.params.a
+    const b = req.params.b
+    var parsed1 = parseInt(a, 10);
+    var parsed2 = parseInt(b, 10);
+    res.send('result => ' + (parsed1 + parsed2))
+}
+app.get('/cal/:a/:b', calculate)
 
 app.post('/apppost', (req, res) => res.send('post 1234'))
 
