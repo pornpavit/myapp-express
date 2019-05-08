7c59e033a680a72aa690decbc51bcf56490d1485
toom pornpavit <pornpavit@odds.team>
2019-05-08 15:40:08 +0700

    Add handlerApp

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const handlerApp = (req, res) => {
+    console.log(req.params.name)
+    res.send(`hello  ` + req.params.name)
+}
 
-app.get('/', (req, res) => res.send('hello world yeyyyyyyyyy'))
+app.get('/app/:name', handlerApp)
+
+app.post('/apppost', (req, res) => res.send('post 1234'))
 
 app.listen(port, () => console.log(`exaple app port ${port}!`))
